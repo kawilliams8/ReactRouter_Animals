463836c521c49a9ad32896a8b63c76cc95477a83
Katie Williams <kawilliams8@gmail.com>
2019-08-22 13:47:23 -0600

    Complete lesson

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const router = (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
+
+ReactDOM.render(router, document.getElementById('root'));
 registerServiceWorker();
